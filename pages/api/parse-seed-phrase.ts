d2913ef35706d814d545824ee23e7c9b711137f9
Sai C <saichaitanya88@gmail.com>
2022-04-09 11:12:29 -0400

    parse seed phrase

---
+++ pages/api/parse-seed-phrase.ts
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { KeyPair } from "near-api-js";
+
+type Data = {
+    name: string
+}
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data|Error>
+) {
+    res.status(500).json({ name: "Error", message: "Not implemented" });
+}
