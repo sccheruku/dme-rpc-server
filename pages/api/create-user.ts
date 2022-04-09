3749c317823740f179dfd9e1478fc44002645666
Sai C <saichaitanya88@gmail.com>
2022-04-09 11:20:44 -0400

    add parse seed endpoint

--- pages/api/create-user.ts
+++ pages/api/create-user.ts
@@ -1,26 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-const nearSeedPhrase = require('near-seed-phrase');
+
 
 type Data = {
-    seedPhrase: string,
-    secretKey: string,
-    publicKey: string,
+    name: string
 }
 
 export default function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data | Error>
 ) {
-    if (req.method != "POST"){
-        return res.status(500).json({ name: "Error", message: "Not implemented" });
-    }
-    
-    const seedPhrase = req.body;
-    const { secretKey, publicKey } = nearSeedPhrase.parseSeedPhrase(seedPhrase)
-    return {
-        seedPhrase,
-        secretKey,
-        publicKey
-    }
+    res.status(500).json({ name: "Error", message: "Not implemented" });
 }
--- pages/api/parse-seed-phrase.ts
+++ pages/api/parse-seed-phrase.ts
@@ -1,14 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { KeyPair } from "near-api-js";
+const nearSeedPhrase = require('near-seed-phrase');
 
 type Data = {
-    name: string
+    seedPhrase: string,
+    secretKey: string,
+    publicKey: string,
 }
 
 export default function handler(
     req: NextApiRequest,
-    res: NextApiResponse<Data|Error>
+    res: NextApiResponse<Data | Error>
 ) {
-    res.status(500).json({ name: "Error", message: "Not implemented" });
+    if (req.method != "POST") {
+        return res.status(500).json({ name: "Error", message: "Not implemented" });
+    }
+    console.log("GET REQUEST")
+    const payload = req.body;
+    console.log("seedPhrase", payload.seedPhrase)
+    const { seedPhrase, secretKey, publicKey } = nearSeedPhrase.parseSeedPhrase(payload.seedPhrase)
+    return res.status(200).json({
+        seedPhrase,
+        secretKey,
+        publicKey
+    });
 }
